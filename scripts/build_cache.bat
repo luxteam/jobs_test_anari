026635cc2562979c9f820a7d33fa775155e532b5
Speedwag00n <INemankov@luxoft.com>
2022-05-04 15:09:42 +0300

    Update name of anari test tool

--- scripts/build_cache.bat
+++ scripts/build_cache.bat
@@ -3,4 +3,4 @@ set LIBRARY="%1"
 
 python -m pip install -r ../jobs_launcher/install/requirements.txt
 
-python build_cache.py --tool "..\Anari\anari_regression_tests.exe" --library %LIBRARY%
+python build_cache.py --tool "..\Anari\anariRenderTests.exe" --library %LIBRARY%
--- scripts/build_cache.sh
+++ scripts/build_cache.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 LIBRARY="$1"
 
-python3.9 build_cache.py --tool "../Anari/anari_regression_tests" --library $LIBRARY
+python3.9 build_cache.py --tool "../Anari/anariRenderTests" --library $LIBRARY
