e0cbf4ce27ace6ff181334d8f26907e4d0ba3fe6
Speedwag00n <INemankov@luxoft.com>
2022-06-18 15:33:09 +0300

    Update local config

--- local_config.py
+++ local_config.py
@@ -2,6 +2,10 @@ tool_name = 'anari'
 report_type = 'default'
 show_skipped_groups = True
 tracked_metrics_files_number = 10
-tracked_metrics = {}
+tracked_metrics = {
+    'render_time': {'displaying_name': 'Render time', 'function': 'sum', 'displaying_unit': 's'}
+}
+tracked_metrics_charts_location = 'performance'
+tracked_metrics_files_number = 10000
 analyze_render_time = {"max_diff": 0.05}
 show_render_time = True
